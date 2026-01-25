b836beee6d967d6b813f6fc34a56ed5c83e79515
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-25 14:22:28 +0000

    chore(deps): update terraform terraform-ibm-modules/base-ocp-vpc/ibm to v3.78.5

--- examples/openshift/main.tf
+++ examples/openshift/main.tf
@@ -72,7 +72,7 @@ locals {
 module "ocp_base" {
   count                = var.cluster_name_id == null ? 1 : 0
   source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version              = "3.78.4"
+  version              = "3.78.5"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
   tags                 = var.resource_tags
