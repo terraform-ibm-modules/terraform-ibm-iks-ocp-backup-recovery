dabdfc780a82b20a0706e661c76d1efd44d3ee4b
Nitish S Hanchinagoudra <Nitish.S.Hanchinagoudra@ibm.com>
2026-02-26 14:33:32 +0530

    refactor: Replace token-based authentication with client certificate and key for Kubernetes and Helm providers.

--- examples/openshift-classic/provider.tf
+++ examples/openshift-classic/provider.tf
@@ -10,7 +10,8 @@ provider "ibm" {
 # Kubernetes provider for ROKS Classic cluster
 provider "kubernetes" {
   host                   = data.ibm_container_cluster_config.cluster_config.host
-  token                  = data.ibm_container_cluster_config.cluster_config.token
+  client_certificate     = data.ibm_container_cluster_config.cluster_config.admin_certificate
+  client_key             = data.ibm_container_cluster_config.cluster_config.admin_key
   cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
 }
 
@@ -18,7 +19,8 @@ provider "kubernetes" {
 provider "helm" {
   kubernetes = {
     host                   = data.ibm_container_cluster_config.cluster_config.host
-    token                  = data.ibm_container_cluster_config.cluster_config.token
+    client_certificate     = data.ibm_container_cluster_config.cluster_config.admin_certificate
+    client_key             = data.ibm_container_cluster_config.cluster_config.admin_key
     cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
   }
 
--- solutions/fully-configurable/provider.tf
+++ solutions/fully-configurable/provider.tf
@@ -9,14 +9,16 @@ provider "ibm" {
 
 provider "kubernetes" {
   host                   = data.ibm_container_cluster_config.cluster_config.host
-  token                  = data.ibm_container_cluster_config.cluster_config.token
+  client_certificate     = data.ibm_container_cluster_config.cluster_config.admin_certificate
+  client_key             = data.ibm_container_cluster_config.cluster_config.admin_key
   cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
 }
 
 provider "helm" {
   kubernetes = {
     host                   = data.ibm_container_cluster_config.cluster_config.host
-    token                  = data.ibm_container_cluster_config.cluster_config.token
+    client_certificate     = data.ibm_container_cluster_config.cluster_config.admin_certificate
+    client_key             = data.ibm_container_cluster_config.cluster_config.admin_key
     cluster_ca_certificate = data.ibm_container_cluster_config.cluster_config.ca_certificate
   }
   registries = [
