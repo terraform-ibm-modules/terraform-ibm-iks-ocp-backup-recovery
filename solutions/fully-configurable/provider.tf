946da56b97cbf6f40a8bb00381ffe6c49b30b1a3
Nitish S Hanchinagoudra <Nitish.S.Hanchinagoudra@ibm.com>
2026-03-16 17:59:10 +0530

    feat(iam-endpoint): add IBM Cloud IAM endpoint configuration with regional support

--- solutions/fully-configurable/provider.tf
+++ solutions/fully-configurable/provider.tf
@@ -5,7 +5,7 @@
 provider "ibm" {
   ibmcloud_api_key    = var.ibmcloud_api_key
   visibility          = var.provider_visibility
-  endpoints_file_path = "endpoints-testcloud.json"
+  endpoints_file_path = var.dsc_registry == "stg.icr.io" ? "${path.module}/endpoints-testcloud.json" : "${path.module}/endpoints.json"
 }
 
 provider "kubernetes" {
