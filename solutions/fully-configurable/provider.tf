508b7f2308af4b7a98a0559faf30404d2d2f5851
Nitish S Hanchinagoudra <Nitish.S.Hanchinagoudra@ibm.com>
2026-03-17 13:54:31 +0530

    refactor(iam-endpoint): hardcode testcloud endpoints file path

--- solutions/fully-configurable/provider.tf
+++ solutions/fully-configurable/provider.tf
@@ -5,7 +5,7 @@
 provider "ibm" {
   ibmcloud_api_key    = var.ibmcloud_api_key
   visibility          = var.provider_visibility
-  endpoints_file_path = strcontains(var.dsc_registry, "stg") ? "endpoints-testcloud.json" : "endpoints.json"
+  endpoints_file_path = "endpoints-testcloud.json"
 }
 
 provider "kubernetes" {
