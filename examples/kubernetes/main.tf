798bb7c88076969bfb2acbfac9b4baf60e68c6a0
Shivashish Ratnam <shivashishratnam@gmail.com>
2026-05-06 09:04:23 +0530

    create_kubeconfig_dir in IKs

--- examples/kubernetes/main.tf
+++ examples/kubernetes/main.tf
@@ -120,11 +120,19 @@ data "ibm_container_cluster" "classic_cluster_data" {
   resource_group_id = module.resource_group.resource_group_id
 }
 
+resource "terraform_data" "create_kubeconfig_dir" {
+  provisioner "local-exec" {
+    command = "mkdir -p ${path.module}/kubeconfig"
+  }
+}
+
 data "ibm_container_cluster_config" "cluster_config" {
   cluster_name_id   = local.cluster_id
   resource_group_id = module.resource_group.resource_group_id
   admin             = true
   config_dir        = "${path.module}/kubeconfig"
+
+  depends_on = [terraform_data.create_kubeconfig_dir]
 }
 
 # Sleep to allow RBAC sync on cluster
