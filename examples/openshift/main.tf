04753ce55d1f5ae7edbad1050d33c0063faefb4b
Nitish S Hanchinagoudra <Nitish.S.Hanchinagoudra@ibm.com>
2026-07-20 20:12:09 +0530

    feat: add disable_outbound_traffic_protection to ocp_base module in openshift example

--- examples/openshift/main.tf
+++ examples/openshift/main.tf
@@ -83,20 +83,21 @@ locals {
 }
 
 module "ocp_base" {
-  count                = var.cluster_name_id == null && !var.classic_cluster ? 1 : 0
-  source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version              = "3.87.1"
-  resource_group_id    = module.resource_group.resource_group_id
-  region               = var.region
-  tags                 = var.resource_tags
-  cluster_name         = "${var.prefix}-cluster"
-  force_delete_storage = true
-  vpc_id               = ibm_is_vpc.vpc[0].id
-  vpc_subnets          = local.cluster_vpc_subnets
-  ocp_version          = var.ocp_version
-  worker_pools         = local.worker_pools
-  access_tags          = var.access_tags
-  ocp_entitlement      = var.ocp_entitlement
+  count                               = var.cluster_name_id == null && !var.classic_cluster ? 1 : 0
+  source                              = "terraform-ibm-modules/base-ocp-vpc/ibm"
+  version                             = "3.87.1"
+  resource_group_id                   = module.resource_group.resource_group_id
+  region                              = var.region
+  tags                                = var.resource_tags
+  cluster_name                        = "${var.prefix}-cluster"
+  force_delete_storage                = true
+  vpc_id                              = ibm_is_vpc.vpc[0].id
+  vpc_subnets                         = local.cluster_vpc_subnets
+  ocp_version                         = var.ocp_version
+  worker_pools                        = local.worker_pools
+  access_tags                         = var.access_tags
+  ocp_entitlement                     = var.ocp_entitlement
+  disable_outbound_traffic_protection = true
 }
 
 # Lookup the current default OpenShift version
