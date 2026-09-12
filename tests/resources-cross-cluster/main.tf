a5bf9f57cd8c502eefbedbd6b429085262a99c5b
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-09-12 21:34:15 +0000

    fix(deps): update dependencies

--- tests/resources-cross-cluster/main.tf
+++ tests/resources-cross-cluster/main.tf
@@ -4,7 +4,7 @@
 
 module "resource_group" {
   source                       = "terraform-ibm-modules/resource-group/ibm"
-  version                      = "1.6.0"
+  version                      = "1.6.1"
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
 }
@@ -16,7 +16,7 @@ locals {
 
 module "crn_parser" {
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version = "1.5.0"
+  version = "1.9.0"
   count   = local.existing_brs_instance_crn == null ? 0 : 1
   crn     = local.existing_brs_instance_crn
 }
@@ -24,7 +24,7 @@ module "crn_parser" {
 # Pre-provision Source Connection in existing BRS instance
 module "source_connection" {
   source                    = "terraform-ibm-modules/backup-recovery/ibm"
-  version                   = "1.12.3"
+  version                   = "1.12.4"
   region                    = local.brs_region
   resource_group_id         = module.resource_group.resource_group_id
   ibmcloud_api_key          = var.ibmcloud_api_key
@@ -40,7 +40,7 @@ module "source_connection" {
 # Pre-provision Target Connection in existing BRS instance
 module "target_connection" {
   source                    = "terraform-ibm-modules/backup-recovery/ibm"
-  version                   = "1.12.3"
+  version                   = "1.12.4"
   region                    = local.brs_region
   resource_group_id         = module.resource_group.resource_group_id
   ibmcloud_api_key          = var.ibmcloud_api_key
